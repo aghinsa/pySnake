7ddc87ee13abc53bf43def18e60dc4ed2c18977a
Aghin Shah Alin <aghinsa@gmail.com>
2019-12-26 00:05:26 +0530

    moving length

--- assets.py
+++ assets.py
@@ -1,18 +1,61 @@
 
 class Snake:
-    x,y=(10,10)
-    speed = 1
+    x = []
+    y = []
+    step = 44
+    direction = 0
+    length = 3
+
+    update_count = 0
+    update_count_max = 1
+
+    def __init__(self,length):
+        self.length = length
+        for _ in range(length):
+            self.x.append(0)
+            self.y.append(0)
+
+
+    def update(self):
+
+        self.update_count+=1
+        if self.update_count > self.update_count_max:
+            for i in range(self.length-1,0,-1):
+                self.x[i]=self.x[i-1]
+                self.y[i]=self.y[i-1]
+
+            if self.direction == 0:
+                self.x[0] += self.step
+            elif self.direction == 1:
+                self.x[0] -= self.step
+            elif self.direction == 2:
+                self.y[0] -= self.step
+            elif self.direction == 3:
+                self.y[0] += self.step
+            else:
+                raise Exception
+
+            self.update_count = 0
 
     def moveRight(self):
-        self.x += self.speed
+        self.direction = 0
+
     def moveLeft(self):
-        self.x -= self.speed
+        self.direction = 1        
 
     def moveUp(self):
-        self.y -= self.speed
+        self.direction = 2
+
     def moveDown(self):
-        self.y += self.speed
+        self.direction = 3        
 
     @property
     def position(self):
-        return(self.x,self.y)
+        return(self.x[0],self.y[0])
+    
+    def draw(self,surface,image):
+        for i in range(self.length):
+            surface.blit(image,
+                (self.x[i]/2.0,self.y[i]/2.0)
+            )
+            
--- window.py
+++ window.py
@@ -1,7 +1,11 @@
-from pygame.locals import *
 import pygame
+import time
+
+from pygame.locals import *
 from dataclasses import dataclass
 from typing import Any
+
+
 @dataclass
 class WindowConfig:
     height : int 
@@ -11,7 +15,7 @@ class WindowConfig:
 class Window:
     def __init__(self,config:WindowConfig)->None:
         self.config = config
-        self.player = self.config.player()
+        self.player = self.config.player(10)
         self._running = True
         self.window_size = (self.config.width,self.config.height)
 
@@ -23,7 +27,6 @@ class Window:
         )
         pygame.display.set_caption('Snake')
         self._running=True
-        obj_size = (10,10)
         self.snake_body = pygame.image.load("src/img/snake.jpg").convert()
 
     def on_event(self,event):
@@ -31,12 +34,11 @@ class Window:
             self._running=False
     
     def on_loop(self):
-        pass
+        self.player.update()
     
     def on_render(self):
         self.display.fill((255,255,255))
-        self.display.blit(self.snake_body,
-                        self.player.position)
+        self.player.draw(self.display,self.snake_body)
         pygame.display.flip()
 
     def on_cleanup(self):
@@ -45,6 +47,7 @@ class Window:
     def on_execute(self):
         if (self.on_init() == False):
             self._running = False
+
         while(self._running):
             pygame.event.pump()
             keys = pygame.key.get_pressed()
@@ -66,7 +69,8 @@ class Window:
 
             self.on_loop()
             self.on_render()
-        
+            time.sleep(50/1000.0)
+
         print("Exiting")
         self.on_cleanup() 
 
